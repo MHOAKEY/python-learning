057b98b135867a858447d8eed3cca0b2afd493a0
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-26 15:46:59 -0300

    display sum of times letter a was found from user input

--- learning.py
+++ learning.py
@@ -1,1 +1,12 @@
-# testtest
+# Write a program that takes in a string and returns how many vowels and consonants are in that string.
+# Function should return: X Vowels X Consonants
+
+# print(*map(userWord.lower().count, "aeiou"))
+
+from asyncio import constants
+
+
+userWord = input("Enter word(s) to display sum of letter A: ")
+aSearch = "a"
+aCount = userWord.count(aSearch)
+print("The letter A has been found: ", aCount)
