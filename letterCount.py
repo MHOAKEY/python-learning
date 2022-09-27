cf7444dea4b354f137fcd0b123d7a5e0606b9216
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-27 15:52:11 -0300

    simplified/some fixes

--- letterCount.py
+++ letterCount.py
@@ -1,28 +1,17 @@
-# Write a program that takes in a string and returns how many vowels and consonants are in that string.
-# Function should return: X Vowels X Consonants
+userWord = input(
+    "Enter word(s) to display sum of vowels and consonants found: ")
 
-# print(*map(userWord.lower().count, "aeiou"))
+remove = userWord.strip(" `1234567890-=~!@#$%^&*()_+,./;'[]\<>?:")
 
-userWord = input("Enter word(s) to display sum of vowels found: ")
+removeSpaces = userWord.count(" ")
 
-vowel = userWord.lower().count("a") + userWord.count("e") + userWord.count(
-    "i") + userWord.count("o") + userWord.count("u") + userWord.count("y")
+vowel = remove.lower().count("a")\
+    + remove.lower().count("e")\
+    + remove.lower().count("i")\
+    + remove.lower().count("o")\
+    + remove.lower().count("u")\
+    + remove.lower().count("y")\
 
-spaces = userWord.count(" ")
+consonants = len(remove) - removeSpaces - vowel
 
-consonants = len(userWord) - vowel - spaces
-
-# count2 = userWord.lower().count("b") + userWord.lower().count("c") \
-#     + userWord.lower().count("d") + userWord.lower().count("f") \
-#     + userWord.lower().count("g") + userWord.lower().count("h") \
-#     + userWord.lower().count("j") + userWord.lower().count("k") \
-#     + userWord.lower().count("l") + userWord.lower().count("m") \
-#     + userWord.lower().count("n") + userWord.lower().count("p") \
-#     + userWord.lower().count("q") + userWord.lower().count("r") \
-#     + userWord.lower().count("s") + userWord.lower().count("t") \
-#     + userWord.lower().count("v") + userWord.lower().count("w") \
-#     + userWord.lower().count("x") + userWord.lower().count("z")
-
-# print(count, "Vowel(s)", count2, "Consonant(s)")
-
-print(vowel, "Vowel(s)", consonants, "Consonant(s)")
+print(vowel, " Vowel(s)", consonants, " Consonant(s)")
