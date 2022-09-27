4cba1938ed27183bc3abe5a69bb170e6f92fac5c
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-27 13:31:27 -0300

    Added count for consonants

--- letterCount.py
+++ letterCount.py
@@ -3,12 +3,16 @@
 
 # print(*map(userWord.lower().count, "aeiou"))
 
-from asyncio import constants
-
-
-userWord = input("Enter word(s) to display sum of letter A: ")
+userWord = input("Enter word(s) to display sum of vowels found: ")
 
 count = userWord.lower().count("a") + userWord.count("e") + userWord.count(
-    "i") + userWord.count("o") + userWord.count("u")
+    "i") + userWord.count("o") + userWord.count("u") + userWord.count("y")
 
-print("Sum of vowels found: ", count)
+count2 = userWord.lower().count("b") + userWord.lower().count("c") \
+    + userWord.lower().count("d") + userWord.lower().count("f") \
+    + userWord.lower().count("g") + userWord.lower().count("h") + userWord.lower().count("j") + userWord.lower().count("k") + userWord.lower().count("l") + userWord.lower().count("m") + \
+    userWord.lower().count("n") + userWord.lower().count("p") + userWord.lower().count("q") + userWord.lower().count("r") + userWord.lower().count("s") + userWord.lower().count("t") + \
+    userWord.lower().count("v") + userWord.lower().count("w") + \
+    userWord.lower().count("x") + userWord.lower().count("z")
+
+print(count, "Vowel(s)", count2, "Consonant(s)")
