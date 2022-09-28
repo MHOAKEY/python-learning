9f677cab7cba748ee1f806774f21e5e502fba3f4
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-28 14:30:59 -0300

    added input2 and starting if statements

--- impMetConvert.py
+++ impMetConvert.py
@@ -9,7 +9,26 @@
 # meters
 # centimeters
 
+# 1 Mile =
+# 1.609344 KM
+# 1609.344  M
+# 160934 CM
+
 userInput = input("Which measurements would you like to convert? \
     \n 1 Miles \n 2 Yards \n 3 Feet \n 4 Inches \n Please enter 1,2,3 or 4: ")
 
-Print(userInput)
+userInput2 = input("Which measurements would you like to convert to? \
+    \n Kilometers \n Meters \n Centimeters \n Please enter 1,2 or 3: ")
+
+
+if userInput == ("1")
+print(userInput2)
+
+if userInput == ("2")
+print(userInput2)
+
+if userInput == ("3")
+print(userInput2)
+
+if userInput == ("4")
+print(userInput2)
