f309be56965f1b9c00bc0d1486cbb5ca2c6a072a
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-28 14:10:33 -0300

    Displaying user options

---
+++ impMetConvert.py
@@ -0,0 +1,15 @@
+# Build a program that can take in imperial lengths and convert them to metric
+# Program will take inputs from the user for:
+# miles
+# yards
+# feet
+# inches
+# and convert them to:
+# kilometers
+# meters
+# centimeters
+
+userInput = input("Which measurements would you like to convert? \
+    \n 1 Miles \n 2 Yards \n 3 Feet \n 4 Inches \n Please enter 1,2,3 or 4: ")
+
+Print(userInput)
