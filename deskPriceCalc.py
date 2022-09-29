619fa7abc373932bc4b277bdd2325b33921c9847
Matt Oake <matt@Matts-MacBook-Air.local>
2022-09-29 14:28:00 -0300

    First commit

---
+++ deskPriceCalc.py
@@ -0,0 +1,9 @@
+# Create a program that calculates the price for custom desks
+# The program will take specifications from the user:
+# length, width, wood type (mahogany, oak, pine), and how many drawers
+# There is a minimum cost of $200 for any desk
+# There is a large surface additional cost of $50 if the surface is over 750 square inches
+# Mahogany has an additional cost of $150, Oak has an additional cost of $125, and Pine is free.
+# Drawers cost $30 each
+
+# the program should take all the inputs from the user and then display the price for the custom desk.
