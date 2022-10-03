1cb14e2f3b2f5b3f509ab7d3921b59a4fc4cf204
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-03 13:27:53 -0300

    Request 5 numbers from user, Create user inputs into a LIST(ARRAY), print ARRAY in reverse back to user

---
+++ playingWithArrays.py
@@ -0,0 +1,23 @@
+# Write a program that takes 5 numbers from a user.
+
+# The program will then place these numbers into an array.
+
+# The program will reverse the list and then print the numbers one at a time back to the user.
+
+# The program will find  the average of all numbers in the array and display the average to the user
+
+# The program will then find and print out all numbers greater than the average
+
+userNum1 = input("Enter number: ")
+
+userNum2 = input("Enter number: ")
+
+userNum3 = input("Enter number: ")
+
+userNum4 = input("Enter number: ")
+
+userNum5 = input("Enter number: ")
+
+userNumArray = [userNum5, userNum4, userNum3, userNum2, userNum1]
+
+print(userNumArray)
