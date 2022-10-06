8c450591865bd0cf2512d6752ca2dd662509ff06
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-06 13:29:01 -0300

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+# Build a Tic Tac Toe game in python
+# - 2 Human players
+# - One player selects a space. If the space is not taken then the space is assigned with X or O depending on the player
+# - If the space is taken then the user is asked to choose a different space
+# - Display the state of the board to the players after each move
+# - You will likely want to check for a win condition after each turn instead of waiting until the end
+# - Make sure that all win conditions are checked for (row, column, diagonal)
+# - Display the winner to the users unless game is a tie, in which case just display that it was a tie.
+
+# Bonus points:
+# - Add ability to start over after game is finished without having to run the program again
+# - Make the game have an Ai that can play a single human player
