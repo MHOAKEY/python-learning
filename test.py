4afc5f15967e15e167b85ace6a63de4657ac6b80
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-13 15:23:34 -0300

    First Commit.

---
+++ test.py
@@ -0,0 +1,28 @@
+# 1. Create a function that reverses a string but does not use any built in Python string functions. The function will take a string and return the reversed string
+
+# example:
+# string = "Good morning"
+# answer = "gninrom dooG"
+
+
+# 2. Given an unsorted array of numbers, find the largest and smallest numbers in the array.
+# Special Rule: You cannot sort the array to accomplish this task.
+
+# Example:
+# array = [ 5, 8, 1, 19, 7, 4, 6 ]
+# answer = Largest: 19 Smallest: 1
+
+
+# 3. Given an array of int and a target int, find the index of the 2 values in the array that sum the target.
+
+# example:
+# intArray = [1,2,3,4,5]
+# targetInt = 5
+# answer = index 1 and 2 or index 0 and 3
+
+
+# 4. Write a function that takes in 2 strings and finds out if they are anagrams of eachother.
+
+# Function must take 2 arguments. Each of them a string.
+# Function must find of if they are anagrams
+# Function must return true or false
