44d5df9c65b5656c7da3b0e4b40b6f0048b9be6b
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-18 13:58:29 -0300

    Complete question #2.

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
+print("\n1:")
 # 1. Create a function that reverses a string but does not use any built in Python string functions. The function will take a string and return the reversed string
 
 # example:
 # string = "Good morning"
+print("string = Good morning")
 # answer = "gninrom dooG"
 
 
 string = "Good morning"
 answer = string[::-1]
 
-print(string)
-print(answer)
+print("answer =", answer)
 
 
 # OR!
@@ -19,20 +20,65 @@ def reverseString(string):
     reverseString = ""
     for eachLetter in string:
         reverseString = eachLetter + reverseString
-    print(reverseString)
+    return reverseString
 
 
-reverseString(answer)
+print("answer =", reverseString(string))
 
 
+print("\n2:")
 # 2. Given an unsorted array of numbers, find the largest and smallest numbers in the array.
 # Special Rule: You cannot sort the array to accomplish this task.
 
 # Example:
 # array = [ 5, 8, 1, 19, 7, 4, 6 ]
+print("array = [ 5, 8, 1, 19, 7, 4, 6 ]")
 # answer = Largest: 19 Smallest: 1
 
 
+array = [5, 8, 1, 19, 7, 4, 6]
+
+largeNum = array[0]
+for num in array[1:]:
+    if num > largeNum:
+        largeNum = num
+# print(largeNum)
+
+smallNum = array[0]
+for num in array[1:]:
+    if num < smallNum:
+        smallNum = num
+# print(smallNum)
+
+print("answer = Largest:", largeNum, "Smallest:", smallNum)
+
+
+# OR!
+
+
+def findLargeNum(numbers):
+    largeNum = numbers[0]
+    for number in numbers[1:]:
+        if number > largeNum:
+            largeNum = number
+    # print(largeNum)
+    return largeNum
+
+
+def findSmallNum(numbers):
+    smallNum = numbers[0]
+    for number in numbers[1:]:
+        if number < smallNum:
+            smallNum = number
+    # print(smallNum)
+    return smallNum
+
+
+print("answer = Largest:", findLargeNum(
+    array), "Smallest:", findSmallNum(array))
+
+
+print("\n3:")
 # 3. Given an array of int and a target int, find the index of the 2 values in the array that sum the target.
 
 # example:
