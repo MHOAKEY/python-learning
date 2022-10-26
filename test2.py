21f1fc0362248c101a1ecd3dd61fb9435809b719
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-26 14:37:52 -0300

    Question 2 complete.

--- test2.py
+++ test2.py
@@ -11,80 +11,33 @@ import time
 # 2022-06-23 this will obviously vary based on what day it currently is)
 
 
-def timeBetweenNowAndThen(dateInput):
-    today = datetime.datetime.now()
-    todayUnix = time.mktime(today.timetuple()) * 1000
-    userYear = dateInput[:4]
-    userMonth = dateInput[5:7]
-    userDay = dateInput[8:11]
-    userYearMonthDay = datetime.date(
-        int(userYear), int(userMonth), int(userDay))
-    userUnixTime = (time.mktime(userYearMonthDay.timetuple()) * 1000)
-    unixBetweenDates = todayUnix - userUnixTime
-    unixYearsBetween = unixBetweenDates / 31_536_000_000
-    unixMonthsBetween = (unixBetweenDates -
-                         (int(unixYearsBetween) * 31_536_000_000)) / 2_629_800_000
-    unixDaysBetween = ((unixBetweenDates - (int(unixYearsBetween) * 31_536_000_000)
-                        ) - (int(unixMonthsBetween) * 2_629_800_000)) / 86_400_000
+# def timeBetweenNowAndThen(dateInput):
+#     today = datetime.datetime.now()
+#     todayUnix = time.mktime(today.timetuple()) * 1000
+#     userYear = dateInput[:4]
+#     userMonth = dateInput[5:7]
+#     userDay = dateInput[8:11]
+#     userYearMonthDay = datetime.date(
+#         int(userYear), int(userMonth), int(userDay))
+#     userUnixTime = (time.mktime(userYearMonthDay.timetuple()) * 1000)
+#     unixBetweenDates = todayUnix - userUnixTime
+#     unixYearsBetween = unixBetweenDates / 31_536_000_000
+#     unixMonthsBetween = (unixBetweenDates -
+#                          (int(unixYearsBetween) * 31_536_000_000)) / 2_629_800_000
+#     unixDaysBetween = ((unixBetweenDates - (int(unixYearsBetween) * 31_536_000_000)
+#                         ) - (int(unixMonthsBetween) * 2_629_800_000)) / 86_400_000
 
-    result = str(int(unixYearsBetween)) + " Year(s), " + str(int(
-        unixMonthsBetween)) + " Month(s), " + str(int(unixDaysBetween)) + " Day(s)"
+#     result = str(int(unixYearsBetween)) + " Year(s), " + str(int(
+#         unixMonthsBetween)) + " Month(s), " + str(int(unixDaysBetween)) + " Day(s)"
 
-    if result.find("-") == -1:
-        return result + " ago."
-    return result.replace("-", "") + " until."
-
-
-print("\nEnter date in format YYYY-MM-DD:")
-userYYYY_MM_DD = input()
-print(timeBetweenNowAndThen(userYYYY_MM_DD))
+#     if result.find("-") == -1:
+#         return result + " ago."
+#     return result.replace("-", "") + " until."
 
 
-# today = datetime.datetime.now()
-# todayYearMonthDay = today.strftime("%Y" + " %m" + " %d")
-# todayUnix = time.mktime(today.timetuple()) * 1000
-# print("\n\nToday is YYYY-MM-DD:", todayYearMonthDay)
-
-
-# print("\nEnter year as YYYY format:")
-# userYear = int(input())
-# print("\nEnter month as MM format:")
-# userMonth = int(input())
-# print("\nEnter Day as DD format:")
-# userDay = int(input())
-
-
-# userYYYYMMDD = input()
-
-
-# userYear = userYYYYMMDD[:4]
-# userMonth = userYYYYMMDD[5:7]
-# userDay = userYYYYMMDD[8:11]
-
-# print(userYear)
-# print(userMonth)
-# print(userDay)
-
-
-# userYearMonthDay = datetime.date(userYear, userMonth, userDay)
-# userUnixTime = (time.mktime(userYearMonthDay.timetuple()) * 1000)
-# unixBetweenDates = todayUnix - userUnixTime
-
-# unixYearsBetween = unixBetweenDates / 31_536_000_000
-# unixMonthsBetween = (unixBetweenDates -
-#                      (int(unixYearsBetween) * 31_536_000_000)) / 2_629_800_000
-# unixDaysBetween = ((unixBetweenDates - (int(unixYearsBetween) * 31_536_000_000)
-#                     ) - (int(unixMonthsBetween) * 2_629_800_000)) / 86_400_000
-
-# print("\nYou have entered:", userYearMonthDay)
-# # print("Today Unix:", todayUnix)
-# # print(" User Unix:", userUnixTime)
-# # print("Unix remainder:", unixBetweenDates)
-# print("\n\nTime between today and time entered:")
-# print("Year(s):", int(unixYearsBetween), "Month(s):", int(
-#     unixMonthsBetween), "Day(s):", int(unixDaysBetween), "\n\n")
-# print("Month(s):", int(unixMonthsBetween))
-# print("Day(s):", int(unixDaysBetween), "\n\n")
+# print("\nEnter date in format YYYY-MM-DD:")
+# userYYYY_MM_DD = input()
+# print(timeBetweenNowAndThen(userYYYY_MM_DD))
 
 
 # 2. Write a function that will find the length of a nested array
@@ -96,6 +49,18 @@ print(timeBetweenNowAndThen(userYYYY_MM_DD))
 # Example
 # input: ["dog", ["apple", "orange"], "frog", "cat"]
 # output: nested array has 2 values
+
+
+array = [1, [1, 2, 3], 2, 3]
+
+
+def findNestCheckLength(array):
+    for i in array:
+        if isinstance(i, list):
+            return len(i)
+
+
+print(findNestCheckLength(array))
 
 # 3. Write a function that will find any prime numbers in a given range.
 # Example input 5,7. The range is from 5 to 7 inclusive in this case. So the function will need to check if 5, 6 or 7 are prime numbers.
